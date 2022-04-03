ac1430ff4d7e8433350985d99507daff49102add
wzh_gitee_20210722 <wzh_gitee_20210722>
2022-04-03 10:02:08 +0800

    远程仓库初始化

---
+++ src/main/java/com/lagou/SpringBootMytestApplication.java
@@ -0,0 +1,21 @@
+package com.lagou;
+
+import com.lagou.config.EnableRegisterServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication//标注在类上说明这个类是`SpringBoot`的主配置类
+//@EnableRegisterServer
+public class SpringBootMytestApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMytestApplication.class, args);
+	}
+
+/*	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 这里要指向核心启动类
+		return builder.sources(SpringBootMytestApplication.class);
+	}*/
+}
---
+++ src/test/java/com/lagou/springbootmytest/SpringBootMytestApplicationTests.java
@@ -0,0 +1,39 @@
+package com.lagou.springbootmytest;
+
+import com.lagou.pojo.SimpleBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMytestApplicationTests {
+
+
+
+	@Autowired
+	private SimpleBean simpleBean;
+
+	@Test
+	public  void contextLoads() {
+		System.out.println(simpleBean);
+	}
+
+
+	@Autowired
+	DataSource dataSource;
+
+
+	@Test
+	public void test1() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
+	}
+
+}
