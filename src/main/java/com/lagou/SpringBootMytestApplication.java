229463dd8465f714ebbb4c226958553d0a3dc072
wzh_gitee_20210722 <wzh_gitee_20210722>
2022-04-03 10:23:09 +0800

    提交22222222222

--- src/main/java/com/lagou/SpringBootMytestApplication.java
+++ src/main/java/com/lagou/SpringBootMytestApplication.java
@@ -12,6 +12,8 @@ public class SpringBootMytestApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMytestApplication.class, args);
 		System.out.println("------------11111111111");
+
+        System.out.println("------------22222222222");
 	}
 
 /*	@Override
